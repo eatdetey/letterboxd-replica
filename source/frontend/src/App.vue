41486e59b38720931d6136dc19609b88ec25af74
lotarv <lotarev200445@gmail.com>
2026-03-03 18:25:09 +0300

    feat(frontend): up vuetify

--- source/frontend/src/App.vue
+++ source/frontend/src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from '~/features/Home/HelloWorld.vue'
+import Home from '~/features/Home/Home.vue'
 </script>
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+          </a>
+          <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+          </a>
+        </div>
+        <Home msg="Vite + Vue" />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
