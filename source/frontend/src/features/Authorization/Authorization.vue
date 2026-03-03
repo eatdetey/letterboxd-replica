939fa4a2dddf170499a80273895310a052c1ed78
lotarv <lotarev200445@gmail.com>
2026-03-03 18:35:36 +0300

    feat(frontend): up vue-router

---
+++ source/frontend/src/features/Authorization/Authorization.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Это страница авторизации, если ты не понял</h1>
+  <router-link :to="{name: 'home'}">Хочу на главную</router-link>
+</template>
--- source/frontend/src/main.ts
+++ source/frontend/src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-createApp(App).use(vuetify).mount('#app')
+import router from './router'
+createApp(App)
+    .use(vuetify)
+    .use(router)
+    .mount('#app')
