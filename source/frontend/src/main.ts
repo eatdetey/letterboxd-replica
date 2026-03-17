3ba93e9d3abf95a2f0e2d48ef9068de027d03520
lotarv <lotarev200445@gmail.com>
2026-03-17 18:04:13 +0300

    chore(frontend): configure frontend aliases and pinia

--- source/frontend/src/main.ts
+++ source/frontend/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+
 createApp(App)
-    .use(vuetify)
-    .use(router)
-    .mount('#app')
+  .use(vuetify)
+  .use(router)
+  .use(createPinia())
+  .mount('#app')
