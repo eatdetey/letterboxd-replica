7dbe18c21070202ed2fbccae24f4866330c432f6
lotarv <lotarev200445@gmail.com>
2026-03-17 18:04:46 +0300

    feat(frontend): add shared app layout for header and footer

--- source/frontend/src/App.vue
+++ source/frontend/src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import AppHeader from '~/widgets/header/ui/AppHeader.vue'
+import AppFooter from '~/widgets/footer/ui/AppFooter.vue'
 </script>
+
 <template>
   <v-app>
+    <AppHeader />
     <v-main>
       <router-view></router-view>
     </v-main>
+    <AppFooter />
   </v-app>
 </template>
