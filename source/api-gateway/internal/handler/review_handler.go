eff9fb0f929f05eeb569417bdd499a3ea802f11c
ZaiiiRan <a1b075753@gmail.com>
2026-03-20 20:35:58 +0300

    feat(api-gateway): changes in urls

--- source/api-gateway/internal/handler/review_handler.go
+++ source/api-gateway/internal/handler/review_handler.go
@@ -45,16 +45,41 @@ func (h *ReviewHandler) GetMovieReviews(c fiber.Ctx) error {
 
 	items := make([]reviewResponse, 0, len(resp.Items))
 	for _, item := range resp.Items {
-		items = append(items, reviewResponse{
-			ID:        item.Id,
-			UserID:    item.UserId,
-			Username:  item.Username,
-			Text:      item.Text,
-			CreatedAt: item.CreatedAt,
-		})
+		items = append(items, mapReviewResponse(item))
 	}
 
 	return c.JSON(fiber.Map{"items": items})
+}
+
+func (h *ReviewHandler) GetMovieReviewByID(c fiber.Ctx) error {
+	movieID := strings.TrimSpace(c.Params("id"))
+	if movieID == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "movie id is required")
+	}
+	reviewID := strings.TrimSpace(c.Params("review_id"))
+	if reviewID == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "review id is required")
+	}
+
+	ctx, cancel := buildGRPCContext(c)
+	defer cancel()
+
+	resp, err := h.client.GetReviews(ctx, &reviewpb.GetReviewsRequest{
+		MovieId: movieID,
+	})
+	if err != nil {
+		return grpcErrorToFiber(err)
+	}
+
+	for _, review := range resp.Items {
+		if review.GetId() == reviewID {
+			return c.JSON(fiber.Map{
+				"review": mapReviewResponse(review),
+			})
+		}
+	}
+
+	return fiber.NewError(fiber.StatusNotFound, "review not found")
 }
 
 func (h *ReviewHandler) AddMovieReview(c fiber.Ctx) error {
@@ -82,12 +107,16 @@ func (h *ReviewHandler) AddMovieReview(c fiber.Ctx) error {
 	review := resp.GetReview()
 
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"review": reviewResponse{
-			ID:        review.GetId(),
-			UserID:    review.GetUserId(),
-			Username:  review.GetUsername(),
-			Text:      review.GetText(),
-			CreatedAt: review.GetCreatedAt(),
-		},
+		"review": mapReviewResponse(review),
 	})
+}
+
+func mapReviewResponse(item *reviewpb.Review) reviewResponse {
+	return reviewResponse{
+		ID:        item.GetId(),
+		UserID:    item.GetUserId(),
+		Username:  item.GetUsername(),
+		Text:      item.GetText(),
+		CreatedAt: item.GetCreatedAt(),
+	}
 }
